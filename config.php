7ec3a3457b2a0622cb9ca01d2a7bc6ee27dd6e6b
Votre Nom <votre.email@example.com>
2026-05-05 22:32:07 +0100

    Isoler uniquement les fichiers du module Post/Commentaire

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// config.php
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'nutrimind');
-define('DB_USER', 'root');
-define('DB_PASS', ''); // Mettre le mot de passe si existant (ex: sur WAMP souvent vide)
-define('BASE_URL', '/nutrimind_int/'); // Ajuster selon le nom du dossier dans htdocs/www
